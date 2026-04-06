80c66a60dea3fcb04f5e9be6f620e4bbbf608190
DENDENICH <daniil.gol26@yandex.ru>
2026-04-06 19:19:54 +0300

    - Add checking key in Dao class - Add KVService in tests - Update and correct KVService - Add KVService in Server

--- src/main/java/company/vk/edu/distrib/compute/dendenich/DaoI.java
+++ src/main/java/company/vk/edu/distrib/compute/dendenich/DaoI.java
@@ -1,6 +1,8 @@
 package company.vk.edu.distrib.compute.dendenich;
 
+import java.io.IOException;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentHashMap;
 
 import company.vk.edu.distrib.compute.Dao;
@@ -10,17 +12,30 @@ public class DaoI implements Dao<byte[]> {
     private final Map<String, byte[]> storage = new ConcurrentHashMap<>();
 
     @Override
-    public byte[] get(String key) {
-        return storage.get(key);
+    public byte[] get(String key) throws IllegalArgumentException, NoSuchElementException, IOException {
+        if (key == null || key.isBlank()) {
+            throw new IllegalArgumentException("Key is null or blank");
+        }
+        final var value = storage.get(key);
+        if (value == null) {
+            throw new NoSuchElementException("no value for key " + key);
+        }
+        return value;
     }
 
     @Override
-    public void upsert(String key, byte[] value) {
+    public void upsert(String key, byte[] value) throws IllegalArgumentException, IOException {
+        if (key == null || key.isBlank()) {
+            throw new IllegalArgumentException("Key is null or blank");
+        }
         storage.put(key, value);
     }
 
     @Override
-    public void delete(String key) {
+    public void delete(String key) throws IllegalArgumentException, NoSuchElementException, IOException {
+        if (key == null || key.isBlank()) {
+            throw new IllegalArgumentException("Key is null or blank");
+        }
         storage.remove(key);
     }
 
--- src/main/java/company/vk/edu/distrib/compute/dendenich/KVServiceI.java
+++ src/main/java/company/vk/edu/distrib/compute/dendenich/KVServiceI.java
@@ -4,6 +4,10 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import company.vk.edu.distrib.compute.Dao;
 import company.vk.edu.distrib.compute.KVService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.InputStream;
@@ -11,6 +15,7 @@ import java.net.InetSocketAddress;
 import java.util.NoSuchElementException;
 
 public class KVServiceI implements KVService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KVServiceI.class);
     private final HttpServer server;
     private final Dao<byte[]> dao;
 
@@ -23,17 +28,27 @@ public class KVServiceI implements KVService {
         server.createContext("/v0/status", this::handleStatus);
         server.createContext("/v0/entity", this::handleEntity);
 
-        // В Enterprise-системах важно задавать Executor (пул потоков)
+        // любой запрос, не попавший в верхние пути - будет выброшен с ошибкой
+        server.createContext("/", exchange -> {
+            try (exchange) {
+                exchange.sendResponseHeaders(400, -1);
+            }
+        });
+
+        // Задаются Executor (пул потоков)
         server.setExecutor(java.util.concurrent.Executors.newFixedThreadPool(8));
     }
 
     @Override
     public void start() {
+        LOGGER.info("Server starting");
         server.start();
     }
 
+    // Завершаем работу сервера, вместе с
     @Override
     public void stop() {
+        LOGGER.info("Server shutdown");
         server.stop(0);
     }
 
@@ -43,53 +58,82 @@ public class KVServiceI implements KVService {
             // Возвращаем 200 OK в нормальной ситуации
             exchange.sendResponseHeaders(200, -1);
         } else {
-            exchange.sendResponseHeaders(405, -1); // Method Not Allowed
+            exchange.sendResponseHeaders(503, -1); // Server error
         }
         exchange.close();
     }
 
     private void handleEntity(HttpExchange exchange) throws IOException {
         try (exchange) {
-            String method = exchange.getRequestMethod();
-            String query = exchange.getRequestURI().getQuery();
-            String id = extractId(query);
+            try {
+                String method = exchange.getRequestMethod();
+                String query = exchange.getRequestURI().getQuery();
+                String id = extractId(query);
 
-            if (id == null || id.isEmpty()) {
+                switch (method) {
+                    case "GET" -> {
+                        LOGGER.info("Begin start GET query");
+                        byte[] data = dao.get(id);
+                        exchange.sendResponseHeaders(200, data.length);
+                        try (OutputStream os = exchange.getResponseBody()) {
+                            os.write(data);
+                        }
+                    }
+                    case "PUT" -> {
+                        try (InputStream is = exchange.getRequestBody()) {
+                            LOGGER.info("Begin start PUT query");
+                            byte[] body = is.readAllBytes();
+                            dao.upsert(id, body);
+                        }
+                        exchange.sendResponseHeaders(201, -1);
+                    }
+                    case "DELETE" -> {
+                        LOGGER.info("Begin start DELETE query");
+                        dao.delete(id);
+                        exchange.sendResponseHeaders(202, -1);
+                    }
+                    default -> exchange.sendResponseHeaders(405, -1);
+                }
+            } catch (NoSuchElementException e) {
+                LOGGER.error("NoSuchElementException");
+                exchange.sendResponseHeaders(404, -1);
+            } catch (IllegalArgumentException e) {
+                LOGGER.error("IllegalArgumentException");
                 exchange.sendResponseHeaders(400, -1);
-                return;
+            } catch (IOException e) {
+                LOGGER.error("IOException");
+                exchange.sendResponseHeaders(500, -1);
             }
 
-            switch (method) {
-                case "GET" -> {
-                    byte[] data = dao.get(id);
-                    exchange.sendResponseHeaders(200, data.length);
-                    try (OutputStream os = exchange.getResponseBody()) {
-                        os.write(data);
-                    }
-                }
-                case "PUT" -> {
-                    try (InputStream is = exchange.getRequestBody()) {
-                        byte[] body = is.readAllBytes();
-                        dao.upsert(id, body);
-                    }
-                    exchange.sendResponseHeaders(201, -1);
-                }
-                case "DELETE" -> {
-                    dao.delete(id);
-                    exchange.sendResponseHeaders(202, -1);
-                }
-                default -> exchange.sendResponseHeaders(405, -1);
-            }
-        } catch (NoSuchElementException e) {
-            exchange.sendResponseHeaders(404, -1);
         }
     }
 
-    // Вспомогательный метод для парсинга ?id=<ID>
     private String extractId(String query) {
-        if (query != null && query.startsWith("id=")) {
-            return query.substring(3);
+        LOGGER.info("Get id from query ");
+
+        if (query == null || query.isBlank()) {
+            LOGGER.error("Param 'id' is not in query");
+            throw new IllegalArgumentException("Missing query");
         }
-        return null;
+
+        if (!query.startsWith("id=")) {
+            LOGGER.error("Param 'id' is not in query");
+            throw new IllegalArgumentException("Missing id parameter");
+        }
+
+        if (query.contains("&")) {
+            LOGGER.error("Unexpected parameters");
+            throw new IllegalArgumentException("Unexpected parameters");
+        }
+
+        String param = query.substring(3);
+
+        if (param.isEmpty()) {
+            throw new IllegalArgumentException("Empty id");
+        }
+
+        LOGGER.debug("Get id from query");
+        return param;
+
     }
 }
